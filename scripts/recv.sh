6cfaf8ad793e3741fa5cc588a388e53faf163434
zhongjiechen97 <chenzhongjie97@outlook.com>
2023-05-18 11:02:26 +0800

    fff

--- scripts/recv.sh
+++ scripts/recv.sh
@@ -3,8 +3,9 @@ app='../isochron rcv'
 CPU=5
 args='--step_threshold 0.00002'
 
-ptp4l -i $interface -2 -P --step_threshold 0.00002 &
-phc2sys -a -rr &
+sudo ptp4l -i $interface -2 -P --step_threshold 0.00002 &
+# sudo phc2sys -a -rr &
+sudo phc2sys -s CLOCK_REALTIME -c $interface -m -O 0 &
 taskset -c 5 $app \
 	--interface $interface \
 	--quiet \
